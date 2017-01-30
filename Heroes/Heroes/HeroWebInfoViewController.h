8c5b1971272e8f340b42bf0a49177ba4fa95e4f3
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-01-30 07:33:57 -0500

    Storyboard for webViewVC, fixed scrolling on character info for detailVC

---
+++ Heroes/Heroes/HeroWebInfoViewController.h
@@ -0,0 +1,15 @@
+//
+//  HeroWebInfoViewController.h
+//  Heroes
+//
+//  Created by Joanna Lingenfelter on 1/30/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeroWebInfoViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UIWebView *webView;
+
+@end
